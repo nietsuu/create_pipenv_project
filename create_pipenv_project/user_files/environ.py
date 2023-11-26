dc932cb5a1eae0fd140409bc3d3dac14d13c39eb
Nie Tsuu <nietsuu@gmail.com>
2023-11-26 15:59:23 +0800

    Improve environ.py

--- create_pipenv_project/user_files/environ.py
+++ create_pipenv_project/user_files/environ.py
@@ -1,3 +1,23 @@
 import os
+from typing import cast, Callable, TypeVar
 
-LOGGING_LEVEL: str = os.environ["LOGGING_LEVEL"]
+T = TypeVar("T")
+
+
+def env(t: Callable[[str], T], key: str) -> T:
+    value = os.environ[key]
+
+    if t is bool:
+        value = value.lower().strip()
+
+        for i in ("0", "false", "f", "no", "n"):
+            if value == i:
+                return cast(T, False)
+
+        return cast(T, True)
+
+    return t(value)
+
+
+LOGGING_LEVEL: str = env(str, "LOGGING_LEVEL")
+FILE_LOGGING: bool = env(bool, "FILE_LOGGING")
